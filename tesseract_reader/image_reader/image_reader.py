707f91c9ea26f12a5a28d665f1681e8dc10a5799
Dann38 <38dunn@gmail.com>
2023-11-10 12:24:34 +0800

    read base64

--- tesseract_reader/image_reader/image_reader.py
+++ tesseract_reader/image_reader/image_reader.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import base64
 
 
 class ImageReader:
@@ -9,4 +10,10 @@ class ImageReader:
         chunk_arr = np.frombuffer(chunk, dtype=np.uint8)
         image = cv2.imdecode(chunk_arr, cv2.IMREAD_COLOR)
         return image
+
+    def readb64(self, encoded_data) -> np.ndarray:
+        # encoded_data = uri.split(',')[1]
+        nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        return img
 
